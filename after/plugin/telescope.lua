2b992fffc8e15256218dbad62e2ae0e7995437f8
Suyog Patil <suyog0057@gmail.com>
2024-07-03 21:21:39 +0530

    feat: added after folder

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,11 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', function()
+    local search_term = vim.fn.input("Grep > ");
+    print(search_term)
+    builtin.grep_string({ search = search_term });
+end)
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>pr', builtin.lsp_references, {});
+vim.keymap.set('n', '<C-a>s', builtin.lsp_document_symbols, {});
