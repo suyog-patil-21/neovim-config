307089dcccc7e1a0d094776e0fbd5b969e5c0981
Suyog Patil <suyog0057@gmail.com>
2024-07-03 22:11:09 +0530

    fix: typo in remap for cwnext

--- lua/suyogconfig/remap.lua
+++ lua/suyogconfig/remap.lua
@@ -19,7 +19,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end)
 
 -- Quick Fix list
-vim.keymap.set("n", "<C-k", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
