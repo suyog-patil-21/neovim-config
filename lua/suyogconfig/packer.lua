d7d8b7b724917870616ee32bdd9728d5aa2dce91
Suyog Patil <suyog0057@gmail.com>
2024-07-09 16:42:25 +0530

    add: lualine.nvim package

--- lua/suyogconfig/packer.lua
+++ lua/suyogconfig/packer.lua
@@ -5,52 +5,55 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-	-- telescope
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.6',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    -- telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-	use({
-		'rose-pine/neovim',as = 'rose-pine', config = function()
-			vim.cmd('colorscheme rose-pine')
-		end})
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
 
-	-- treesitter
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    -- treesitter
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
-	-- harpoon
-	use ('ThePrimeagen/harpoon')
+    -- harpoon
+    use('ThePrimeagen/harpoon')
 
-	-- undotree
-	use('mbbill/undotree')
+    -- undotree
+    use('mbbill/undotree')
 
-	-- fugitive for git
-	use('tpope/vim-fugitive')
+    -- fugitive for git
+    use('tpope/vim-fugitive')
 
-	-- mason config 
-	use {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	}
+    -- mason config
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
 
-	-- autocomplete 
-	use { "hrsh7th/nvim-cmp",
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-path",
-	"saadparwaiz1/cmp_luasnip"
-	}
+    -- autocomplete
+    use { "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "saadparwaiz1/cmp_luasnip"
+    }
 
-	-- snipet 
-	use {
-	"L3MON4D3/LuaSnip",
-	"rafamadriz/friendly-snippets"
-	}
+    -- snipet
+    use {
+        "L3MON4D3/LuaSnip",
+        "rafamadriz/friendly-snippets"
+    }
 
 
     -- flutter Lsp config
@@ -64,6 +67,10 @@ return require('packer').startup(function(use)
 
     -- multiline cursor like visual studio code
     use 'mg979/vim-visual-multi'
+
+    -- Bottom statusline
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 end)
-
-
