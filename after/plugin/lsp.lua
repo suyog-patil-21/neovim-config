d85f0a7175d4e39a527e89ed8f7f274b038e13bd
suyog-patil-21 <suyog0057@gmail.com>
2025-07-16 12:58:24 +0530

    feat: Add flutter tool keymapping

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,7 +10,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
         vim.keymap.set('n', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+        -- vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
 
         -- already configured in telescope file
         -- vim.keymap.set('n', '<leader>gr', function() vim.lsp.buf.references() end, opts)
