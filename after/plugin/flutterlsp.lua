36910865c6d1f90e2990c91dd5ea60326c1791c2
suyog-patil-21 <suyog0057@gmail.com>
2025-07-16 16:32:00 +0530

    fix: flutter lsp hotReload

--- after/plugin/flutterlsp.lua
+++ after/plugin/flutterlsp.lua
@@ -2,7 +2,7 @@ require("flutter-tools").setup {} -- use defaults
 
 vim.keymap.set("n", "<leader>fr", "<cmd>FlutterRun<cr>", { desc = "Flutter Run" })
 vim.keymap.set("n", "<leader>fq", "<cmd>FlutterQuit<cr>", { desc = "Flutter Quit" })
-vim.keymap.set("n", "<leader>fh", "<cmd>FlutterHotReload<cr>", { desc = "Flutter Hot Reload" })
+vim.keymap.set("n", "<leader>fh", "<cmd>FlutterReload<cr>", { desc = "Flutter Hot Reload" })
 vim.keymap.set("n", "<leader>fR", "<cmd>FlutterRestart<cr>", { desc = "Flutter Hot Restart" })
 vim.keymap.set("n", "<leader>fd", "<cmd>FlutterDevices<cr>", { desc = "Flutter Devices" })
 vim.keymap.set("n", "<leader>fe", "<cmd>FlutterEmulators<cr>", { desc = "Flutter Emulators" })
